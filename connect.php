4c06ea2231e5c7e46c62170afc3d540e41ea57b6
Фёдор <88681809+777alz@users.noreply.github.com>
2023-05-10 18:00:55 +0100

    MySQL files

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php // connect.php allows connection to the database
+    $hn='';
+	$db = '';
+	$un = '';
+	$pw = '';
+	
+	$conn = new mysqli($hn, $un,$pw,$db);
+	
+	if ($conn->connect_error)
+		{ die($conn->connect_error);
+		echo '<br>';
+		echo 'Unfortunately you could not be connected to the database
+		      please check you have the correct credentials';
+	}
+	else 
+	{	
+		echo '<br>';
+		echo 'You have connected to the databse successfully <br><br>';
+		
+	};
+
+   ?>
